1e8e6e41fb1106e5ee79ba24f7e347d3690af0c8
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-04-02 18:06:03 -0400

    Modify companies.html and script.js, finish addCompany() function

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function addCompany() {
-    let companyURL = prompt(`Copy and paste URL of company logo:`);
-    console.log(companyURL);
-}
-
-function company(name, url) {
-    this.name = name;
-    this.url = url;
+    let logoURL = prompt(`Copy and paste URL of company logo:`);
+    console.log(logoURL);
+    let logo = document.createElement("IMG");
+    logo.src = logoURL;
+    document.getElementById("company-logos").appendChild(logo);
 }
